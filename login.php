39e78470dc1d4e9b95760fdb2fc3d0e5638191a5
Jannat Hossain <70332589+JannatHossain24@users.noreply.github.com>
2022-04-23 16:33:32 +0600

    admin

--- login.php
+++ login.php
@@ -1,76 +1,196 @@
- <?php
- require_once ("include/initialize.php"); 
- if (@$_GET['page'] <= 2 or @$_GET['page'] > 5) {
-  # code...
-    // unset($_SESSION['PRODUCTID']);
-    // // unset($_SESSION['QTY']);
-    // // unset($_SESSION['TOTAL']);
+<?php
+require_once("../include/initialize.php");
+
+ ?>
+  <?php
+ // login confirmation
+  if(isset($_SESSION['USERID'])){
+    redirect(web_root."admin/index.php");
+  }
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Bachelor of Science and Entrepreneurs</title>
+
+<!-- Bootstrap core CSS -->
+<link href="<?php echo web_root; ?>css/bootstrap.min.css" rel="stylesheet">
+<link href="<?php echo web_root; ?>css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+<link href="<?php echo web_root; ?>css/dataTables.bootstrap.css" rel="stylesheet" media="screen">
+<link rel="stylesheet" type="text/css" href="<?php echo web_root; ?>css/jquery.dataTables.css"> 
+<link href="<?php echo web_root; ?>css/bootstrap.css" rel="stylesheet" media="screen">
+
+<link href="<?php echo web_root; ?>fonts/font-awesome.min.css" rel="stylesheet" media="screen">
+<!-- Plugins -->
+<script type="text/javascript" language="javascript" src="<?php echo web_root; ?>js/jquery.js"></script>
+<script type="text/javascript" language="javascript" src="<?php echo web_root; ?>js/jquery.dataTables.js"></script>
+<!-- <script type="text/javascript" language="javascript" src="<?php echo web_root; ?>js/fixnmix.js"></script> / -->
+
+
+<style>
+@CHARSET "UTF-8";
+/*
+over-ride "Weak" message, show font in dark grey
+*/
+
+.progress-bar {
+    color: #333;
 } 
 
+/*
+Reference:
+http://www.bootstrapzen.com/item/135/simple-login-form-logo/
+*/
 
+* {
+    -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+  outline: none;
+}
+
+    .form-control {
+    position: relative;
+    font-size: 16px;
+    height: auto;
+    padding: 10px;
+    @include box-sizing(border-box);
+
+    &:focus {
+      z-index: 2;
+    }
+  }
+
+body {
+  /*background: url(../img/bgadmin5.jpg) no-repeat center center fixed;*/
+  background-color: #543e3d;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+}
+
+.login-form {
+  margin-top: 60px;
+}
+
+form[role=login] {
+  color: #5d5d5d;
+  background: #f2f2f2;
+  padding: 26px;
+  border-radius: 10px;
+  -moz-border-radius: 10px;
+  -webkit-border-radius: 10px;
+}
+  form[role=login] img {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 35px;
+    height: 90px;
+  }
+  form[role=login] input,
+  form[role=login] button {
+    font-size: 18px;
+    margin: 16px 0;
+  }
+  form[role=login] > div {
+    text-align: center;
+  }
+  
+.form-links {
+  text-align: center;
+  margin-top: 1em;
+  margin-bottom: 50px;
+}
+  .form-links a {
+    color: #fff;
+  }
  
-if(isset($_POST['sidebarLogin'])){
-  $email = trim($_POST['U_USERNAME']);
-  $upass  = trim($_POST['U_PASS']);
+</style>
+ 
+<body  >
+
+
+<div class="container">
+  
+  <div class="row" id="pwd-container">
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4">
+
+      <section class="login-form"> 
+        <form method="post" action="" role="login">
+          <img src="../img/adminlogo.png" class="img-responsive" alt="" />
+         <!--   <img src="../images/kcclogo.jpg" height="25px" class="img-responsive" alt="" /> -->
+         <?php echo check_message(); ?>
+          <input type="input" name="user_email" placeholder="Username" required class="form-control input-lg" value="" />
+          
+          <input type="password" name="user_pass" class="form-control input-lg" id="password" placeholder="Password" required  />
+          
+          
+          <div class="pwstrength_viewport_progress"></div>
+          
+          
+          <button type="submit" name="btnLogin" class="btn btn-lg btn-primary btn-block">Sign in</button>
+          <!-- <div>
+            <a href="#">Create account</a> or <a href="#">reset password</a>
+          </div>
+           -->
+        </form>
+        
+        <div class="form-links">
+          <!-- <a href="#">www.website.com</a> -->
+        </div>
+      </section>  
+      </div>
+      
+      <div class="col-md-4"></div>
+      
+
+  </div>
+  
+     
+  
+  
+</div>
+   
+</body>
+
+ <?php 
+
+if(isset($_POST['btnLogin'])){
+  $email = trim($_POST['user_email']);
+  $upass  = trim($_POST['user_pass']);
   $h_upass = sha1($upass);
   
    if ($email == '' OR $upass == '') {
 
       message("Invalid Username and Password!", "error");
-      redirect(web_root."index.php");
+      redirect("login.php");
          
-    } else {   
-        $cus = new Customer();
-        $cusres = $cus::cusAuthentication($email,$h_upass);
-
-        if ($cusres==true){
-
-
-           redirect(web_root."index.php?q=profile");
-        }else{
-             message("Invalid Username and Password! Please contact administrator", "error");
-             redirect(web_root."index.php");
-        }
- 
+    } else {  
+  //it creates a new objects of member
+    $user = new User();
+    //make use of the static function, and we passed to parameters
+    $res = $user::userAuthentication($email, $h_upass);
+    if ($res==true) { 
+       message("You logon as ".$_SESSION['U_ROLE'].".","success");
+      if ($_SESSION['U_ROLE']=='Administrator'){
+         redirect(web_root."admin/index.php");
+      }else{
+           redirect(web_root."admin/login.php");
+      }
+    }else{
+      message("Account does not exist! Please contact Administrator.", "error");
+       redirect(web_root."admin/login.php"); 
+    }
  }
-}
-
-
-
- if(isset($_POST['modalLogin'])){
-  $email = trim($_POST['U_USERNAME']);
-  $upass  = trim($_POST['U_PASS']);
-  $h_upass = sha1($upass);
-  
-   if ($email == '' OR $upass == '') { 
-      message("Invalid Username and Password!", "error");
-       redirect(web_root."index.php?page=6");
-         
-    } else {   
-        $cus = new Customer();
-        $cusres = $cus::cusAuthentication($email,$h_upass);
-
-        if ($cusres==true){
-
-           if($_POST['proid']==''){
-            redirect(web_root."index.php?q=orderdetails");
-           }else{
-              $proid = $_POST['proid'];
-              $id = mysql_insert_id(); 
-              $query ="INSERT INTO `tblwishlist` (`PROID`, `CUSID`, `WISHDATE`, `WISHSTATS`)  VALUES ('". $proid."','".$_SESSION['CUSID']."','".DATE('Y-m-d')."',0)";
-              mysql_query($query) or die(mysql_error());
-              redirect(web_root."index.php?q=profile");
-             }
-
-         
-        }else{
-             message("Invalid Username and Password! Please contact administrator", "error");
-             redirect(web_root."index.php");
-        }
- 
- }
- }
+ } 
  ?> 
- 
-
- 
+</head>
+</html>
